1ba481e57fe1335a199686b6a477bae2bb18fe04
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-10-12 20:58:49 -0700

    added screens and navigators

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import ExploreScreen from './screens/ExploreScreen';
+import UserScreen from './screens/UserScreen';
+import { IconButton } from 'react-native-paper';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Explore"
+          screenOptions={({ navigation }) => ({
+            headerRight: () => (
+              <IconButton
+                icon="account"
+                size={25}
+                onPress={() => navigation.navigate('User')}
+              />
+            ),
+            headerStyle: { backgroundColor: '#6200ee' },
+            headerTintColor: '#fff',
+          })}
+        >
+          <Stack.Screen 
+            name="Explore" 
+            component={ExploreScreen} 
+            options={{ title: 'Explore' }} 
+          />
+          <Stack.Screen 
+            name="User" 
+            component={UserScreen} 
+            options={{ title: 'User Profile' }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
